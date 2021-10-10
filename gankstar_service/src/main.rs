ce3d736681c7777a2c43ca6f65f064e2a9f1aeb2
Albin Eneroth <albin.eneroth@gmail.com>
2021-10-10 14:07:20 +0200

    refactor: parse auth key for api

--- gankstar_service/src/main.rs
+++ gankstar_service/src/main.rs
@@ -6,15 +6,12 @@ use std::time::Duration;
 use std::env;
 use std::path::Path;
 use std::fs;
-use std::net::SocketAddr;
-use std::error::Error;
 
 fn watch() -> notify::Result<()> {
 
     // TODO: In production this should be working dir, find a way to use this in dev and production.
     let mut working_dir = env::current_exe()?;
     //REALLY??? while not __retail__?
-    working_dir.pop();
     working_dir.pop();
     working_dir.pop();
     working_dir.pop();
@@ -74,15 +71,26 @@ fn watch() -> notify::Result<()> {
     }
 }
 
+fn get_token(data: &String) -> String {
+  let pattern = r#"["addon_token"] = "#;
+  let start_index = data.find(pattern); 
+  let uuid_size = 39;
+  let mut addon_token: String = data.chars().skip(start_index.unwrap() + pattern.chars().count()).take(uuid_size).collect();
+  addon_token = addon_token.replace(&['"', '\\', ','][..], "");
+  println!("\n{:?}\n", addon_token);
+  return addon_token;
+}
+
 fn handle_event(path: &Path) {
   println!("Should handle event: {:?}", path);
   let data = fs::read_to_string(path).expect("Unable to read file");
-
+  let token = get_token(&data);
   let client = reqwest::blocking::Client::new();
 
-  let params = [("auth_token", "3272a63c-ae10-4170-9453-6b15b1b7aee5"), ("body", &data)];
-  //TODO: add auth token from file as params
-  let res = client.post("https://ganksta.rs/api/receive")
+  // 3272a63c-ae10-4170-9453-6b15b1b7aee5 <- production
+  // 2f817e70-8a4b-4d5e-9138-9fa87099d18b <- dev
+  let params = [("auth_token", &token), ("body", &data)];
+  let res = client.post("http://127.0.0.1:3000/api/receive")
       .form(&params)
       .send();
 
