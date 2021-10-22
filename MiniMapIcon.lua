cb5d43d60c484acd1c47279d44d63fef47f1fd49
Albin Eneroth <albin.eneroth@gmail.com>
2021-10-22 22:28:48 +0200

    refactor: change where we save minimap icon

--- MiniMapIcon.lua
+++ MiniMapIcon.lua
@@ -1,12 +1,12 @@
 local addon = LibStub("AceAddon-3.0"):NewAddon("GankstarsDB", "AceConsole-3.0")
-local bunnyLDB = LibStub("LibDataBroker-1.1"):NewDataObject("Bunnies!", {
+local bunnyLDB = LibStub("LibDataBroker-1.1"):NewDataObject("GankstarsDB", {
 type = "data source",
-text = "Bunnies!",
+text = "Gankstars",
 icon = "Interface\\ICONS\\Ability_DemonHunter_MetamorphasisTank",
 OnClick = function() Gankstars:ToggleFrame() end,
 })
 local icon = LibStub("LibDBIcon-1.0")
 
-function addon:OnInitialize() self.db = LibStub("AceDB-3.0"):New("GankstarsDB", { profile = { minimap = { hide = false, }, }, }) icon:Register("Bunnies!", bunnyLDB, self.db.profile.minimap) self:RegisterChatCommand("gankstars", "CommandTheBunnies") end
+function addon:OnInitialize() self.db = LibStub("AceDB-3.0"):New("GankstarsDB", { profile = { minimap = { hide = false, }, }, }) icon:Register("GankstarsDB", bunnyLDB, self.db.profile.minimap) self:RegisterChatCommand("gankstars", "ToggleGankstars") end
 
-function addon:CommandTheBunnies() self.db.profile.minimap.hide = not self.db.profile.minimap.hide if self.db.profile.minimap.hide then icon:Hide("Bunnies!") else icon:Show("Bunnies!") end end
+function addon:ToggleGankstars() self.db.profile.minimap.hide = not self.db.profile.minimap.hide if self.db.profile.minimap.hide then icon:Hide("GankstarsDB") else icon:Show("GankstarsDB") end end
